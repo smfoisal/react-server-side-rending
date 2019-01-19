f38936deec301ac1b063acc93242028fa0d519de
smfoisal <shaheen.foisal@gmail.com>
2019-01-19 21:27:49 +0600

    first test component rendered

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require ('express');
 const React = require ('react');
 const renderToString = require ('react-dom/server').renderToString;
-const Home = require ('./cilent/components/Home').default;
+const Home = require ('./client/components/Home').default;
 
 const app = express ();
 
 app.get('/', (req, res) => {
     const content = renderToString(<Home />);
-    
+
     res.send(content);
 });
 
